e9312fe00ff926090ef5c4fc988aa6b9a0dd7d71
Angelo Souza <angelo.souza@goomer.com.br>
2020-02-10 09:14:32 -0300

    add setup script

---
+++ setup.sh
@@ -0,0 +1,47 @@
+# VSCode
+snap install --classic code
+
+#Curl
+apt install curl
+
+#ZSH
+apt install zsh
+
+#Oh My ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt install fonts-powerline
+
+#Mongo DB
+apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list #Just for Ubuntu 18.04
+apt-get update
+apt-get install -y mongodb-org
+
+#DBeaver
+snap install dbeaver-ce
+
+#Postman
+snap install postman
+
+#Slack
+snap install slack --classic
+
+#NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+
+#Docker
+apt update
+apt install apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+
+#Spotify
+snap install spotify
+
+# chsh -s $(which zsh) // Command that are not working in script
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" // Can't add this at zshrc filey
